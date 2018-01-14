15f36dbf5ed573940eb1f3f4435c925f0a953e55
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2018-01-14 11:44:09 +0530

    overloading main function

---
+++ main_overload.java
@@ -0,0 +1,18 @@
+class A
+{
+     public static void main(int x)
+     {
+        System.out.println(x);
+      }
+      public static void main(char x)
+     {
+        System.out.println(x);
+      }
+      public static void main(String[] x)
+     {
+        String s[]={"shahnawaz","irfan"};
+	main(20);
+	main('c');
+	main(s);
+	}
+      }
