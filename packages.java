168018764744010e494f0a850ab790d2a244694e
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2018-02-11 09:47:49 +0530

    simple program to understand packages

---
+++ packages.java
@@ -0,0 +1,8 @@
+package com.prolog;
+class packages
+{
+	public static void main(String[] x)
+	{
+		System.out.println("Package is understod");
+	}
+}
---
+++ packages2.java
@@ -0,0 +1,16 @@
+package com.prolog.shahnawaz;
+
+class packages2
+{
+	public static void main(String[] x)
+	{
+		System.out.println("Package is understod");
+	}
+}
+class A
+{
+
+}
+class B
+{
+}
