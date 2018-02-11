ed66586248c278d8d4f2f47732c325d0f30e86bf
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2018-02-11 09:43:52 +0530

    program to understand abstraction

---
+++ Abstraction.java
@@ -0,0 +1,31 @@
+abstract class abstraction1
+{  
+	 abstract void area();
+}
+abstract class abstraction2 extends abstraction1
+{
+	abstract void perimeter();
+}
+class abstraction extends abstraction2 
+{
+      int length=10,breadth=20;
+	  void area()
+	{ 
+		  int area;
+		  area=length*breadth;
+		  System.out.println("Area of rectangle="+area);
+	}
+	void perimeter()
+	{
+		int peri;
+		peri=2*(this.length+this.breadth);
+		System.out.println("Perimeter of rectangle="+peri);
+	}
+	public static void main(String[] x)
+	{
+		abstraction abs=new abstraction();
+		abs.area();
+		abs.perimeter();
+	}
+}
+	
