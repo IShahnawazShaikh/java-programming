198c4a45026085622a998de1207bd14fc4f0f552
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2018-02-13 21:43:05 +0530

    program to override toString method
    toString method is executed by object class when we don't override this method.It is good practice to override this method according to our need.

---
+++ object1.java
@@ -0,0 +1,17 @@
+class object1
+{
+	 object1()
+     	{
+		       super();
+              System.out.println("constructor -2");
+		}
+	 public String toString()
+	     {
+		       return "Shahnawaz shaikh";
+	     }
+	  public static void main(String[] x)
+	      {
+		        object1 o=new object1();
+				System.out.println(o);
+		  }
+}
