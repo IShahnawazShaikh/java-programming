924063bad3ae2f536185dbf7b421232e72b5e984
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2018-04-02 22:06:44 +0530

    Object Graph in serialization

---
+++ Serialization3.java
@@ -0,0 +1,32 @@
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+class A implements Serializable{
+	String value="We";
+     B b=new B();
+}
+class B implements Serializable{
+	  String value="are";
+     C c=new C();
+}
+class C implements Serializable{
+    String value="Prologian";
+}
+class Serialization3{
+	public static void main(String[] c)throws FileNotFoundException,IOException,ClassNotFoundException{
+		 A a=new A();
+		 FileOutputStream fos=new FileOutputStream("Shahnawaz.ser");
+		 ObjectOutputStream oos=new ObjectOutputStream(fos);
+		 oos.writeObject(a);
+		 FileInputStream fis=new FileInputStream("shahnawaz.ser");
+		 ObjectInputStream ois=new ObjectInputStream(fis);
+		 A obj=(A)ois.readObject();
+		 System.out.println(obj.value);
+		 System.out.println(obj.b.value);
+		 System.out.println(obj.b.c.value);
+	}
+}
