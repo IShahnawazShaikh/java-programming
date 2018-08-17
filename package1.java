cf3dd8ca7b952254c3e9392296a2b700687ee93b
Shahnawaz Shaikh <32239343+shahnawazshaikh123@users.noreply.github.com>
2018-08-17 23:50:45 +0530

    Understanding some concepts of package

---
+++ package1.java
@@ -0,0 +1,10 @@
+package com.jamia.pack.jamians;
+class package1
+{ 
+	   void display(){
+          System.out.println("shahnawaz");
+	   }
+	  public static void main(String...c){
+           new package1().display();
+	  }
+}
---
+++ package2.java
@@ -0,0 +1,18 @@
+//keep all classes in one package
+package com.java.Jamians;
+class package2
+{
+	  public static void main(String...x){
+		     
+	  }
+}
+class A
+{
+
+}
+class B
+{
+}
+class C
+{
+}
