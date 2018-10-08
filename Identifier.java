22cb00cbd7384855735040f1201022b32b0c0970
Shahnawaz Shaikh <32239343+shahnawazshaikh123@users.noreply.github.com>
2018-10-09 00:54:30 +0530

    Concept of Identifier in java

---
+++ Identifier.java
@@ -0,0 +1,10 @@
+class Identifier
+{
+	public static void main(String...x){
+     
+	  double Integer=10.5;
+	  float Runnable=12.4f;
+	  System.out.println("Showing Class can be use as a Identifier="+Integer);
+	  System.out.println("Showing interfaces can be use as a Identifier="+Runnable);
+	}
+}
