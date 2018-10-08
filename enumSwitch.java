04d8966136f971c4026e60b6ceed90ed43851033
Shahnawaz Shaikh <32239343+shahnawazshaikh123@users.noreply.github.com>
2018-10-09 00:56:18 +0530

    enum vs switch

---
+++ enumSwitch.java
@@ -0,0 +1,28 @@
+enum Week{
+ SUN,MON,TUE,WED;
+}
+class enumDemo3
+{
+	public static void main(String...x){
+          Week w=Week.MON;
+		  switch(w){
+          case SUN:
+			       System.out.println("today is sunday");
+		           break;
+		  case MON:
+			       System.out.println("today is monday");
+		           break;
+			case TUE:
+			       System.out.println("today is tuesday");
+		           break;
+		   case WED:
+			       System.out.println("today is thursday");
+		           break;
+				   case shahnawaz:
+			       System.out.println("today is thursday");
+		           break;
+				   default:
+					    System.out.println("Not Present");
+		  }
+	}
+}
