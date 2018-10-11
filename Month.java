1a4e1b2b90e70b09bbe0f84ac878185cbf7df264
Shahnawaz Shaikh <32239343+shahnawazshaikh123@users.noreply.github.com>
2018-10-11 22:42:33 +0530

    Declaring main method in enum.

---
+++ Month.java
@@ -0,0 +1,15 @@
+enum Month{
+ ;
+  public static void main(String...x){
+     for(String string : x){
+     System.out.println(string);
+	 m1();
+	 }
+ }
+ public static void m1(){
+      System.out.println("In m-1");
+ }
+    public static void display(){
+  System.out.println("shahnawaz");
+}
+}
