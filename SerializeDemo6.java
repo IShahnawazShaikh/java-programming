9b5cf9b1bc84fdb42c7326455cf0ee906c3be026
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2019-01-13 20:58:18 +0530

    Serialization with respect to Inheritance

---
+++ SerializeDemo6.java
@@ -0,0 +1,28 @@
+import java.io.*;
+class Gparent implements Serializable{
+    int data1=10;
+}
+class parent extends Gparent{
+    int data2=20;
+}
+class child extends parent{
+    int data3=30;
+}
+class SerializeDemo6{
+	 public static void main(String[] args)throws Exception
+	  {  
+	  	
+	 	   child ch=new child();
+	 	  FileOutputStream fos=new FileOutputStream("shahnawaz3.ser");
+	 	  ObjectOutputStream oos=new ObjectOutputStream(fos);
+	 	  oos.writeObject(ch);
+	 	  
+	 	   FileInputStream fis=new FileInputStream("shahnawaz3.ser"); 
+     	   ObjectInputStream ois=new ObjectInputStream(fis);
+           child ch1=(child)ois.readObject();
+           System.out.println("child"+ch1.data3);
+           System.out.println("parent"+ch1.data2);
+           System.out.println("Gparent"+ch1.data3);
+
+     	  }
+     	}
