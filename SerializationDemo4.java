f3f59b77dcf73a2381db578131f614d9bbc029ac
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2019-01-13 20:50:37 +0530

    Serialization with respect to inheritance

---
+++ SerializationDemo4.java
@@ -0,0 +1,41 @@
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+class parent {
+    int parentData=10;
+    
+}
+class child extends parent implements Serializable{
+    int childData=20;
+    int parentData=100;
+    void display(){
+    	System.out.println("Jamian");
+    }
+    int parentDatadisplay(){
+    	 return super.parentData;
+    }
+ }
+class SerializationDemo4{
+	public static void main(String[] c)throws FileNotFoundException,IOException,ClassNotFoundException{
+		 child obj1=new child();
+		 FileOutputStream fos=new FileOutputStream("File3.ser");
+		 ObjectOutputStream oos=new ObjectOutputStream(fos);
+		 obj1.display();
+		 System.out.println("Child data="+obj1.childData);
+		 System.out.println("parent data="+obj1.parentData);
+		 System.out.println("parent data="+obj1.parentData);
+		 oos.writeObject(obj1);
+        
+		 FileInputStream fis=new FileInputStream("File3.ser");
+		 ObjectInputStream ois=new ObjectInputStream(fis);
+		 child obj2=(child)ois.readObject();
+		 obj2.display();
+		 System.out.println("Child data="+obj2.childData);
+		 System.out.println("Child parent data="+obj2.parentData);
+		 System.out.println("parent data="+obj2.parentDatadisplay());
+	}
+}
